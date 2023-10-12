5a79d0a5ba80c5b4d49cad8f6105cbbaa9af3fb6
Ellie <intricatebread@gmail.com>
2023-10-12 19:40:54 -0300

    New switch statement syntax

--- src/main/java/me/axialeaa/colorfulminihud/ColorfulLines.java
+++ src/main/java/me/axialeaa/colorfulminihud/ColorfulLines.java
@@ -131,61 +131,66 @@ public class ColorfulLines
     BlockPos pos = new BlockPos(x, y, z);
     ChunkPos chunkPos = new ChunkPos(pos);
 
-    switch(type)
+    return switch(type)
     {
-      case FPS:
-        return line(Formats.FPS_FORMAT, va("fps", fps));
+      case FPS -> line(Formats.FPS_FORMAT, va("fps", fps));
 
-      case MEMORY_USAGE:
+      case MEMORY_USAGE ->
+      {
         long memMax = Runtime.getRuntime().maxMemory();
         long memTotal = Runtime.getRuntime().totalMemory();
         long memFree = Runtime.getRuntime().freeMemory();
         long memUsed = memTotal - memFree;
-        return line(Formats.MEMORY_USAGE_FORMAT,
+        yield line(Formats.MEMORY_USAGE_FORMAT,
           va("pused", "2d", memUsed * 100L / memMax),
           va("pallocated", "2d", memTotal * 100L / memMax),
           va("used", "3d", MiscUtils.bytesToMb(memUsed)),
           va("max", "3d", MiscUtils.bytesToMb(memMax)),
           va("total", "3d", MiscUtils.bytesToMb(memTotal)));
+      }
 
-      case TIME_REAL:
-        Date date = new Date(System.currentTimeMillis());
-        return line(Formats.TIME_REAL_FORMAT, va("time", "tk", date));
+      case TIME_REAL -> line(Formats.TIME_REAL_FORMAT, va("time", "tk", new Date(System.currentTimeMillis())));
 
-      case TIME_WORLD:
-        return line(Formats.TIME_WORLD_FORMAT,
-          va("time", "5d", level.getDayTime()),
-          va("total", level.getGameTime()));
+      case TIME_WORLD -> line(Formats.TIME_WORLD_FORMAT,
+        va("time", "5d", level.getDayTime()),
+        va("total", level.getGameTime()));
 
-      case TIME_WORLD_FORMATTED:
+      case TIME_WORLD_FORMATTED ->
+      {
         long timeDay = level.getDayTime();
         long day = timeDay / 24000;
         int dayTicks = (int)(timeDay % 24000);
-        return line(Formats.TIME_WORLD_FORMATTED_FORMAT,
+        yield line(Formats.TIME_WORLD_FORMATTED_FORMAT,
           va("day", day),
           va("day_1", day + 1),
           va("hour", "02d", (dayTicks / 1000 + 6) % 24),
           va("min", "02d", (int)(dayTicks * 0.06) % 60),
           va("sec", "02d", (int)(dayTicks * 3.6) % 60));
+      }
 
-      case TIME_DAY_MODULO:
+      case TIME_DAY_MODULO ->
+      {
         int mod = Configs.Generic.TIME_DAY_DIVISOR.getIntegerValue();
-        return line(Formats.TIME_DAY_MODULO_FORMAT,
+        yield line(Formats.TIME_DAY_MODULO_FORMAT,
           va("mod", mod),
           va("time", "5d", level.getDayTime() % mod));
+      }
 
-      case TIME_TOTAL_MODULO:
+      case TIME_TOTAL_MODULO ->
+      {
         int mod1 = Configs.Generic.TIME_TOTAL_DIVISOR.getIntegerValue();
-        return line(Formats.TIME_TOTAL_MODULO_FORMAT,
+        yield line(Formats.TIME_TOTAL_MODULO_FORMAT,
           va("mod", mod1),
           va("time", "5d", level.getGameTime() % mod1));
+      }
 
-      case SERVER_TPS:
+      case SERVER_TPS ->
+      {
         if(mc.hasSingleplayerServer() && mc.getSingleplayerServer().getTickCount() % 10 == 0)
           data.updateIntegratedServerTPS();
 
         if(!data.hasTPSData())
-          return line(Formats.SERVER_TPS_NULL_FORMAT);
+          yield line(Formats.SERVER_TPS_NULL_FORMAT);
 
         double tps = data.getServerTPS();
         double mspt = data.getServerMSPT();
@@ -197,7 +202,7 @@ public class ColorfulLines
             mspt <= 45 ? GuiBase.TXT_YELLOW :
             mspt <= 50 ? GuiBase.TXT_GOLD :
             GuiBase.TXT_RED;
-          return line(Formats.SERVER_TPS_CARPET_FORMAT,
+          yield line(Formats.SERVER_TPS_CARPET_FORMAT,
             va("preTps", preTps),
             va("tps", ".1f", tps),
             va("rst", GuiBase.TXT_RST),
@@ -206,21 +211,24 @@ public class ColorfulLines
         }
 
         String preMspt = mspt <= 51 ? GuiBase.TXT_GREEN : GuiBase.TXT_RED;
-        return line(Formats.SERVER_TPS_VANILLA_FORMAT,
+        yield line(Formats.SERVER_TPS_VANILLA_FORMAT,
           va("preTps", preTps),
           va("tps", ".1f", tps),
           va("rst", GuiBase.TXT_RST),
           va("preMspt", preMspt),
           va("mspt", ".1f", mspt));
+      }
 
-      case PING:
+      case PING ->
+      {
         PlayerInfo info = player.connection.getPlayerInfo(player.getUUID());
-        return info == null ? null : line(Formats.PING_FORMAT, va("ping", info.getLatency()));
+        yield info == null ? null : line(Formats.PING_FORMAT, va("ping", info.getLatency()));
+      }
 
-      case COORDINATES:
-      case DIMENSION:
+      case COORDINATES, DIMENSION ->
+      {
         if(addedTypes.contains(InfoToggle.COORDINATES) || addedTypes.contains(InfoToggle.DIMENSION))
-          return null;
+          yield null;
 
         StringBuilder str = new StringBuilder(128);
         str.append("[");
@@ -239,13 +247,13 @@ public class ColorfulLines
           str.append(line(Formats.DIMENSION_FORMAT, va("dim", level.dimension().location().toString())));
           addedTypes.add(InfoToggle.DIMENSION);
         }
-        return str.append("]").toString();
+        yield str.append("]").toString();
+      }
 
-      case BLOCK_POS:
-      case CHUNK_POS:
-      case REGION_FILE:
+      case BLOCK_POS, CHUNK_POS, REGION_FILE ->
+      {
         if(addedTypes.contains(InfoToggle.BLOCK_POS) || addedTypes.contains(InfoToggle.CHUNK_POS) || addedTypes.contains(InfoToggle.REGION_FILE))
-          return null;
+          yield null;
 
         StringBuilder str1 = new StringBuilder(256);
         str1.append("[");
@@ -279,23 +287,23 @@ public class ColorfulLines
             va("z", pos.getZ() >> 9)));
           addedTypes.add(InfoToggle.REGION_FILE);
         }
-        return str1.append("]").toString();
+        yield str1.append("]").toString();
+      }
 
-      case BLOCK_IN_CHUNK:
-        return line(Formats.BLOCK_IN_CHUNK_FORMAT,
-          va("x", pos.getX() & 0xf),
-          va("y", pos.getY() & 0xf),
-          va("z", pos.getZ() & 0xf),
-          va("cx", chunkPos.x),
-          va("cy", pos.getY() >> 4),
-          va("cz", chunkPos.z));
+      case BLOCK_IN_CHUNK -> line(Formats.BLOCK_IN_CHUNK_FORMAT,
+        va("x", pos.getX() & 0xf),
+        va("y", pos.getY() & 0xf),
+        va("z", pos.getZ() & 0xf),
+        va("cx", chunkPos.x),
+        va("cy", pos.getY() >> 4),
+        va("cz", chunkPos.z));
 
-      case BLOCK_BREAK_SPEED:
-        return line(Formats.BLOCK_BREAK_SPEED_FORMAT, va("bbs", ".2f", data.getBlockBreakingSpeed()));
+      case BLOCK_BREAK_SPEED -> line(Formats.BLOCK_BREAK_SPEED_FORMAT, va("bbs", ".2f", data.getBlockBreakingSpeed()));
 
-      case DISTANCE:
+      case DISTANCE ->
+      {
         Vec3 ref = data.getDistanceReferencePoint();
-        return line(Formats.DISTANCE_FORMAT,
+        yield line(Formats.DISTANCE_FORMAT,
           va("d", ".2f", Math.sqrt(ref.distanceToSqr(x, y, z))),
           va("dx", ".2f", x - ref.x),
           va("dy", ".2f", y - ref.y),
@@ -303,8 +311,10 @@ public class ColorfulLines
           va("rx", ".2f", ref.x),
           va("ry", ".2f", ref.y),
           va("rz", ".2f", ref.z));
+      }
 
-      case FACING:
+      case FACING ->
+      {
         Direction facing = player.getDirection();
         String str2 = switch(facing)
         {
@@ -314,34 +324,40 @@ public class ColorfulLines
           case WEST -> Formats.FACING_NX_FORMAT.getStringValue();
           default -> "Invalid direction";
         };
-        return line(Formats.FACING_FORMAT, va("dir", str2));
+        yield line(Formats.FACING_FORMAT, va("dir", str2));
+      }
 
-      case LIGHT_LEVEL:
+      case LIGHT_LEVEL ->
+      {
         if(accessor.getChunkPublic(chunkPos).isEmpty())
-          return null;
+          yield null;
 
         LevelLightEngine lightEngine = level.getChunkSource().getLightEngine();
-        return line(Formats.LIGHT_LEVEL_CLIENT_FORMAT,
+        yield line(Formats.LIGHT_LEVEL_CLIENT_FORMAT,
           va("light", lightEngine.getRawBrightness(pos, 0)),
           va("block", lightEngine.getLayerListener(LightLayer.BLOCK).getLightValue(pos)),
           va("sky", lightEngine.getLayerListener(LightLayer.SKY).getLightValue(pos)));
+      }
 
-      case BEE_COUNT:
+      case BEE_COUNT ->
+      {
         Level bestLevel = WorldUtils.getBestWorld(mc);
         BlockEntity be = accessor.getTargetedBlockEntityPublic(bestLevel, mc);
-        return be instanceof BeehiveBlockEntity bbe ?
+        yield be instanceof BeehiveBlockEntity bbe ?
           line(Formats.BEE_COUNT_FORMAT, va("bees", bbe.getOccupantCount())) : null;
+      }
 
-      case HONEY_LEVEL:
+      case HONEY_LEVEL ->
+      {
         BlockState state = accessor.getTargetedBlockPublic(mc);
-        return state != null && state.getBlock() instanceof BeehiveBlock ?
+        yield state != null && state.getBlock() instanceof BeehiveBlock ?
           line(Formats.HONEY_LEVEL_FORMAT, va("honey", BeehiveBlockEntity.getHoneyLevel(state))) : null;
+      }
 
-      case ROTATION_YAW:
-      case ROTATION_PITCH:
-      case SPEED:
+      case ROTATION_YAW, ROTATION_PITCH, SPEED ->
+      {
         if(addedTypes.contains(InfoToggle.ROTATION_YAW) || addedTypes.contains(InfoToggle.ROTATION_PITCH) || addedTypes.contains(InfoToggle.SPEED))
-          return null;
+          yield null;
 
         StringBuilder str3 = new StringBuilder(128);
         str3.append("[");
@@ -368,63 +384,64 @@ public class ColorfulLines
           str3.append(line(Formats.SPEED_FORMAT, va("speed", dist1 * 20)));
           addedTypes.add(InfoToggle.SPEED);
         }
-        return str3.append("]").toString();
+        yield str3.append("]").toString();
+      }
 
-      case SPEED_AXIS:
-        return line(Formats.SPEED_AXIS_FORMAT,
-          va("x", (x - player.xOld) * 20),
-          va("y", (y - player.yOld) * 20),
-          va("z", (z - player.zOld) * 20));
+      case SPEED_AXIS -> line(Formats.SPEED_AXIS_FORMAT,
+        va("x", (x - player.xOld) * 20),
+        va("y", (y - player.yOld) * 20),
+        va("z", (z - player.zOld) * 20));
 
-      case CHUNK_SECTIONS:
-        return line(Formats.CHUNK_SECTIONS_FORMAT, va("c", ((IMixinWorldRenderer)mc.levelRenderer).getRenderedChunksInvoker()));
+      case CHUNK_SECTIONS -> line(Formats.CHUNK_SECTIONS_FORMAT, va("c", ((IMixinWorldRenderer)mc.levelRenderer).getRenderedChunksInvoker()));
 
-      case CHUNK_SECTIONS_FULL:
-        return line(Formats.CHUNK_SECTIONS_FULL_FORMAT, va("c", mc.levelRenderer.getChunkStatistics()));
+      case CHUNK_SECTIONS_FULL -> line(Formats.CHUNK_SECTIONS_FULL_FORMAT, va("c", mc.levelRenderer.getChunkStatistics()));
 
-      case CHUNK_UPDATES:
-        return "TODO"; // minihud parity
+      case CHUNK_UPDATES -> "TODO"; // minihud parity
 
-      case LOADED_CHUNKS_COUNT:
+      case LOADED_CHUNKS_COUNT ->
+      {
         String chunksClient = level.gatherChunkSourceStats();
         Level levelServer = WorldUtils.getBestWorld(mc);
         if(levelServer == null || levelServer != level)
-          return line(Formats.LOADED_CHUNKS_COUNT_CLIENT_FORMAT, va("client", chunksClient));
+          yield line(Formats.LOADED_CHUNKS_COUNT_CLIENT_FORMAT, va("client", chunksClient));
 
         ServerChunkCache cache = (ServerChunkCache)levelServer.getChunkSource();
-        return line(Formats.LOADED_CHUNKS_COUNT_SERVER_FORMAT,
+        yield line(Formats.LOADED_CHUNKS_COUNT_SERVER_FORMAT,
           va("chunks", cache.getLoadedChunksCount()),
           va("total", cache.getTickingGenerated()),
           va("client", chunksClient));
+      }
 
-      case PARTICLE_COUNT:
-        return line(Formats.PARTICLE_COUNT_FORMAT, va("p", mc.particleEngine.countParticles()));
+      case PARTICLE_COUNT -> line(Formats.PARTICLE_COUNT_FORMAT, va("p", mc.particleEngine.countParticles()));
 
-      case DIFFICULTY:
+      case DIFFICULTY ->
+      {
         ChunkAccess serverChunk = Objects.requireNonNull(level).getChunk(chunkPos.getWorldPosition());
 
         float moonPhaseFactor = level.getMoonBrightness();
         long chunkInhabitedTime = serverChunk.getInhabitedTime();
         DifficultyInstance diff = new DifficultyInstance(level.getDifficulty(), level.getDayTime(), chunkInhabitedTime, moonPhaseFactor);
-        return line(Formats.DIFFICULTY_FORMAT,
+        yield line(Formats.DIFFICULTY_FORMAT,
           va("local", ".2f", diff.getEffectiveDifficulty()),
           va("clamped", ".2f", diff.getSpecialMultiplier()),
           va("day", level.getDayTime() / 24000L)
         );
+      }
 
-      // case BIOME:
-      // case BIOME_REG_NAME:
-      // case TILE_ENTITIES:
-      // case ENTITIES_CLIENT:
-      // case ENTITIES_SERVER:
+      // case BIOME ->
+      // case BIOME_REG_NAME ->
+      // case TILE_ENTITIES ->
+      // case ENTITIES_CLIENT ->
+      // case ENTITIES_SERVER ->
 
-      case SLIME_CHUNK:
+      case SLIME_CHUNK ->
+      {
         if (!MiscUtils.isOverworld(Objects.requireNonNull(level)))
-          return null;
+          yield null;
         if (data.isWorldSeedKnown(level))
         {
           long seed = data.getWorldSeed(level);
-          return line(Formats.SLIME_CHUNK_FORMAT,
+          yield line(Formats.SLIME_CHUNK_FORMAT,
             va("result", MiscUtils.canSlimeSpawnAt(pos.getX(), pos.getZ(), seed) ?
               Formats.SLIME_CHUNK_YES_FORMAT.getStringValue() :
               Formats.SLIME_CHUNK_NO_FORMAT.getStringValue()
@@ -432,18 +449,16 @@ public class ColorfulLines
           );
         }
         else
-        {
-          return line(Formats.SLIME_CHUNK_FORMAT, va("result", Formats.SLIME_CHUNK_NO_SEED_FORMAT.getStringValue()));
-        }
+          yield line(Formats.SLIME_CHUNK_FORMAT, va("result", Formats.SLIME_CHUNK_NO_SEED_FORMAT.getStringValue()));
+      }
 
-      // case LOOKING_AT_ENTITY:
-      // case ENTITY_REG_NAME:
-      // case LOOKING_AT_BLOCK:
-      // case LOOKING_AT_BLOCK_CHUNK:
-      // case BLOCK_PROPS:
+      // case LOOKING_AT_ENTITY ->
+      // case ENTITY_REG_NAME ->
+      // case LOOKING_AT_BLOCK ->
+      // case LOOKING_AT_BLOCK_CHUNK ->
+      // case BLOCK_PROPS ->
 
-      default:
-        return null;
-    }
+      default -> null;
+    };
   }
 }
